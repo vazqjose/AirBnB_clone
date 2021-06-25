4163a9b38e48f6508d443bff45369dacc7f48268
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-25 21:52:34 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 '''comments'''
 
-
 import uuid
 from datetime import datetime, timezone
-
 
 class BaseModel:
     '''comments'''
@@ -15,20 +13,24 @@ class BaseModel:
         created_at: current datetime when an instance is created
         updated_at: current datetime when an instance is updated
         '''
-
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now(timezone.utc).isoformat()
         self.updated_at = datetime.now(timezone.utc).isoformat()
 
     def __str__(self):
         '''
-        Print class nam, id and dictionary info
+                Print class nam, id and dictionary info
         '''
-
         print("[{}] ".format(self.__class__.__name__), end="")
         print("({}) ".format(self.id), end="")
         print("<{}>".format(self.__dict__)
 
     def save(self):
     '''comments'''
+        self.updated_at = datetime.now(timezone.utc).isoformat()
+
+    def to_dict(self):
+    '''comments'''
+        
+
 
