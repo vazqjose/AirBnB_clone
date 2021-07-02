6dc80e52918f1adfd314844aef678cdba98d494e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-02 14:18:42 +0000

    pep8

---
+++ test.c
@@ -0,0 +1,18 @@
+
+
+//"Holberton\0"
+//len = 9
+//these two for loops are the same
+
+for (idx = 0; idx <= len; idx++)
+{
+	_puchar(src[idx]);
+}
+
+for (idx = 0; src[idx] != '\0'; idx++)
+{
+	_puchar(src[idx]);
+}
+
+ear
+
