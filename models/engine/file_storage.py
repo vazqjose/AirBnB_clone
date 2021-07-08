2553e19d61dccd3be523a282d4b7e5b461a09e8b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-08 17:37:09 +0000

    created engine dir, added save method to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,8 @@
 objects created when the programming is running to a file.
 '''
 
+import json
+from models.base_model import BaseModel
 
 class FileStorage:
     '''Class serializes instances to a JSON file and
@@ -20,4 +22,41 @@ class FileStorage:
         reload(self): Deserializes the JSON file to __objects (only if the
         JSON file (__file_path) exists; otherwise, does nothing.
     '''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''Returns the dictionary self.__objects.'''
+        return self.__objects
+
+    def new(self, obj):
+        '''Add the obj to the __objects dictionary with the key:
+        <obj class name>.idx
+        '''
+        key = "{}.".format(obj.__class__.__name__)
+        key += "{}".format(obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        '''Serializes the __objects attribute to a JSON file,
+        the path is stored in the attribute __file_path variable.
+
+        self.__objects is a dictionary of the actual objects, we use
+        a for loop to create a new dictionary that holds the attrs of
+        the objects. We call BaseModel's 'to_dict' method, that
+        returns a dict of all the attrs of the instance and changes datetime
+        attrs into strings before we call the json.dumps method.
+        '''
+        copy_objects_dict = {}
+
+        for key, value in self.__objects.items():
+            copy_objects_dict = value.to_dict()
+
+        with open(self.__file_path, 'w+') as j_file:
+            json.dump(copy_objects_dict, j_file)
+
+    def reload(self):
+        '''
+        '''
+        pass
 
