f3d7ecc8efeff5f8988b5ae84c738370c6469d50
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-08 08:49:34 +0000

    added kwargs to __init__

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
         __str__(self): returns the str representation of the instance:
             [<class name>] (<self.id>) <self.__dict__>
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         id: A string assigned an uuid when an instance is created.
         created_at: A datetime object assigned the current datetime when
@@ -40,6 +40,15 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated at":
+                    dt = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, dt)
+                else:
+                    if key != "__class__":
+                        setattr(self, key, value)
 
     def __str__(self):
         '''Returns the str representation of the instance:
